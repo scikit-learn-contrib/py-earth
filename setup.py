214c87608ca670824ff3066f065dc55d6dd85572
Jason Rudy <jcrudy@gmail.com>
2015-06-06 11:18:46 -0700

    Update setup.py to depend on scikit-learn >= 0.16

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup_args = {'name': 'py-earth',
               'ext_modules': ext_modules,
               'classifiers': ['Development Status :: 3 - Alpha'],
               'requires': ['numpy', 'scipy'],
-              'install_requires': ['scikit-learn',
+              'install_requires': ['scikit-learn >= 0.16',
                                    'sphinx_gallery']}
 
 # Add the build_ext command only if cythonizing
