3fd21dd44171fbf74d8a8f8702b07a6ac8c799d9
Jason Rudy <jcrudy@gmail.com>
2016-08-10 14:48:04 -0700

    Reorganized flow of sympy export and added support for multicolumn models

--- pyearth/export.py
+++ pyearth/export.py
@@ -1,4 +1,3 @@
-import functools
 
 def export_python_function(earth_model):
     """
@@ -39,107 +38,136 @@ def export_python_string(earth_model, function_name="model"):
         yield sum(accessor(x) for accessor in accessors)
     """.format(function_name, ",\n\t\t".join(accessors))
 
-def export_sympy(earth_model):
-    
-    from sympy import Symbol, Add, Mul, Max, RealNumber, Piecewise, Pow, And
+def export_sympy_term_expressions(earth_model):
     """
-    
-    Constructs and exports a sympy expression out of a trained earth model. 
+    Construct a list of sympy expressions for all non-pruned terms in the model.
     
     :param earth_model: Trained pyearth model
-    :return: a sympy expression that accepts an iterator over examples
+    :return: a list of sympy expressions representing terms in the model.  These 
+      expressions are the symbolic equivalent of the Earth.transform method.
 
     """
+    from sympy import Symbol, Add, Mul, Max, RealNumber, Piecewise, Pow, And
+    from ._basis import LinearBasisFunction, HingeBasisFunction, SmoothedHingeBasisFunction, \
+          MissingnessBasisFunction, ConstantBasisFunction
 
-    def termify(bf, i):
-        max_degree = bf.degree()
-        parent = bf
-        parent_name = parent.__str__()
-        bf_dict[parent_name] = i
-        terms = []
-
-        bf_index = bf_dict[parent_name]
-        
-        coef = RealNumber(earth_model.coef_[0][bf_index])
-        terms.append(coef)
-
-        for _ in range(max_degree):
-            variable_index =  parent.get_variable()
-            variable = earth_model.xlabels_[variable_index]
-            bf_var = Symbol(variable)
-            bf_class = str(type(parent))
-
-            if 'LinearBasisFunction' in bf_class:
-                
-                term =  bf_var
-                terms.append(term)
-
-            elif 'SmoothedHingeBasisFunction' in bf_class:
-                
-                knot = RealNumber(parent.get_knot())
-                knot_minus = RealNumber(parent.get_knot_minus())
-                knot_plus = RealNumber(parent.get_knot_plus())
-                r = RealNumber(parent.get_r())
-                p = RealNumber(parent.get_p())
-  
-                if parent.get_reverse() == False:
-                
-                    lower_p = (0, bf_var <= knot_minus)
-                    upper_p = (bf_var - knot, bf_var >= knot_plus)
-                    left_exp = Mul(p, Pow((bf_var - knot_minus), 2))
-                    right_exp = Mul(r, Pow((bf_var - knot_minus), 3))
-                    middle_b = And(knot_minus < bf_var, bf_var < knot_plus)
-                    middle_exp = (Add(left_exp, right_exp), middle_b)
-                    piecewise = Piecewise(lower_p, upper_p, middle_exp)
-                    term = piecewise
-                    
-                    terms.append(term)
-  
-                elif parent.get_reverse() == True:
-#
-                    lower_p = (-(bf_var - knot)), (bf_var <= knot_minus)
-                    upper_p = (0, bf_var >= knot_plus)
-                    left_exp = Mul(p, Pow((bf_var - knot_plus), 2))
-                    right_exp = Mul(r, Pow((bf_var - knot_plus), 3))
-                    middle_b = And(knot_minus < bf_var, bf_var < knot_plus)
-                    middle_exp = (Add(left_exp, right_exp), middle_b)                     
-                    piecewise = Piecewise(lower_p, upper_p, middle_exp)
-                    term = piecewise
-                       
-                    terms.append(term)
-
-            elif 'HingeBasisFunction' in bf_class:
-            
-                
-                knot = parent.get_knot()
-                if parent.get_reverse() == False:
-                    term = Max(0, bf_var - RealNumber(knot))
-                    terms.append(term)
- 
-                elif parent.get_reverse() == True:
-                    term = Max(0, RealNumber(knot) - bf_var)
-                    terms.append(term)
-
-            elif 'MissingnessBasisFunction' in bf_class:
-                print(bf_var)
-
-            else:
-                print(bf_var)
-
-            parent = parent.get_parent()
-        term_list.append(terms)
-
-    # runs through all non-pruned bfs
-    bf_dict = {}
-    i = 0
-    term_list = []
-    for bf in earth_model.basis_:
-        if not bf.is_pruned():
-            termify(bf, i)
-            i += 1
-
-    flatten_return = [functools.reduce(lambda a,b: a * b, item) for item in term_list]
-    expression = functools.reduce(lambda a,b: a + b, flatten_return)
+    def linear_bf_to_factor(bf):
+        return Symbol(bf.label)
     
-    return expression
+    def smoothed_hinge_bf_to_factor(bf):
+        bf_var = Symbol(bf.label)
+        knot = RealNumber(bf.get_knot())
+        knot_minus = RealNumber(bf.get_knot_minus())
+        knot_plus = RealNumber(bf.get_knot_plus())
+        r = RealNumber(bf.get_r())
+        p = RealNumber(bf.get_p())
+        if bf.get_reverse():
+            lower_p = (-(bf_var - knot)), (bf_var <= knot_minus)
+            upper_p = (0, bf_var >= knot_plus)
+            left_exp = Mul(p, Pow((bf_var - knot_plus), 2))
+            right_exp = Mul(r, Pow((bf_var - knot_plus), 3))
+            middle_b = And(knot_minus < bf_var, bf_var < knot_plus)
+            middle_exp = (Add(left_exp, right_exp), middle_b)                     
+            piecewise = Piecewise(lower_p, upper_p, middle_exp)
+            factor = piecewise
+        else:
+            lower_p = (0, bf_var <= knot_minus)
+            upper_p = (bf_var - knot, bf_var >= knot_plus)
+            left_exp = Mul(p, Pow((bf_var - knot_minus), 2))
+            right_exp = Mul(r, Pow((bf_var - knot_minus), 3))
+            middle_b = And(knot_minus < bf_var, bf_var < knot_plus)
+            middle_exp = (Add(left_exp, right_exp), middle_b)
+            piecewise = Piecewise(lower_p, upper_p, middle_exp)
+            factor = piecewise
+        return factor
+    
+    def hinge_bf_to_factor(bf):
+        bf_var = Symbol(bf.label)
+        knot = bf.get_knot()
+        if bf.get_reverse():
+            factor = Max(0, RealNumber(knot) - bf_var)
+        else:
+            factor = Max(0, bf_var - RealNumber(knot))
+        return factor
+    
+    def missingness_bf_to_factor(bf):
+        # This is the error that should be raised when a user attempts to use functionality
+        # that has not yet been implemented.
+        # TODO: Implement this
+        raise NotImplementedError
+    
+    def constant_bf_to_factor(bf):
+        return RealNumber(1)
+    
+    bf_to_factor_dispatcher = {LinearBasisFunction: linear_bf_to_factor,
+                               SmoothedHingeBasisFunction: smoothed_hinge_bf_to_factor,
+                               HingeBasisFunction: hinge_bf_to_factor,
+                               MissingnessBasisFunction: missingness_bf_to_factor,
+                               ConstantBasisFunction: constant_bf_to_factor}
+    
+    def bf_to_factor(bf):
+        '''
+        Convert a BasisFunction to a factor of a term.
+        '''
+        return bf_to_factor_dispatcher[bf.__class__](bf)
+    
+    def bf_to_term(bf):
+        '''
+        Convert a BasisFunction to a term (without coefficient).
+        '''
+        term = bf_to_factor(bf)
+        parent = bf.get_parent()
+        if parent is None:
+            return term
+        else:
+            return Mul(term, bf_to_term(parent))
+    
+    return [bf_to_term(bf) for bf in earth_model.basis_.piter()]
 
+
+def export_sympy(earth_model, columns=None):
+    """
+    Constructs a sympy expression or list of sympy expressions from of a trained earth model. 
+    
+    :param earth_model: Trained pyearth model
+    :param columns: The index or indices of the output columns for which expressions are to 
+      be constructed.  If an integer is used, a sympy expression is returned.  If indices 
+      are given then a list of sympy expressions is returned.  If columns is None, it is treated
+      as if columns=0 for models with only one output column or as columns=slice(None) for more than
+      one output column.
+    :return: a sympy expression or list of sympy expressions equivalent to the Earth.predict method for 
+      the selected output columns.
+
+    """
+    # Set a sane default for columns
+    if columns is None:
+        if earth_model.coef_.shape[0] == 1:
+            columns = 0
+        else:
+            columns = slice(None)
+    
+    # Get basis function terms
+    terms = export_sympy_term_expressions(earth_model)
+    
+    # Handle column choice
+    coefs = earth_model.coef_[columns]
+    if len(coefs.shape) == 1:
+        unwrap = True
+        coefs = [coefs]
+        n_cols = 1
+    else:
+        unwrap = False
+        n_cols = coefs.shape[0]
+    
+    # Combine coefficients with terms for each output column
+    result = [sum([coefs[i][j] * term for j, term in enumerate(terms)]) for i in range(n_cols)]
+    
+    if unwrap:
+        # Result should be an expression rather than a list of expressions.
+        result = result[0]
+    return result
+
+        
+        
+    
+    
