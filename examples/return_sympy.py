dd9bea3cd4c5ddd0adc7eca9d16f21ec3f7d3788
Jason Rudy <jcrudy@gmail.com>
2016-08-10 12:39:29 -0700

    Made example faster and changed text a little

--- examples/return_sympy.py
+++ examples/return_sympy.py
@@ -1,15 +1,13 @@
 """
-============================================
-Returning a sympy expression for a  simple sine function
-============================================
+=====================================================
+Exporting a fitted Earth models as a sympy expression
+=====================================================
 
 A simple example returning a sympy expression describing the fit of a sine function computed by Earth.
 
 """
 
 import numpy
-import matplotlib.pyplot as plt
-
 from pyearth import Earth
 from pyearth import export
 
@@ -17,7 +15,7 @@ from pyearth import export
 numpy.random.seed(2)
 m = 1000
 n = 10
-X = 80 * numpy.random.uniform(size=(m, n)) - 40
+X = 10 * numpy.random.uniform(size=(m, n)) - 40
 y = 100 * \
     (numpy.sin((X[:, 6])) - 4.0) + \
     10 * numpy.random.normal(size=m)
@@ -29,5 +27,6 @@ model.fit(X, y)
 print(model.summary())
 
 #return sympy expression 
+print('Resulting sympy expression:')
 print(export.export_sympy(model))
 
